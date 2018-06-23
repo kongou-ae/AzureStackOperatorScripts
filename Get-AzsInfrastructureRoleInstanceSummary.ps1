03d1197320165690bbc39cee1d3a748b5b8c03dc
kongou-ae <y.matsumoto.ae@gmail.com>
2018-06-23 15:32:19 +0900

    Create Get-AzsInfrastructureRoleInstanceSummary.ps1

---
+++ Get-AzsInfrastructureRoleInstanceSummary.ps1
@@ -0,0 +1,8 @@
+$instances = Get-AzsInfrastructureRoleInstance
+$instances | 
+    Select-Object Name, `
+        State, `
+        @{Label="Cores"; Expression={$_.Size.Cores}}, `
+        @{Label="MemoryGb"; Expression={$_.Size.MemoryGb}}, `
+        @{Label="ScaleUnit"; Expression={[regex]::Match($_.ScaleUnit,"scaleUnits/(.*)").Groups[1].value}}, `
+        @{Label="ScaleUnitNode"; Expression={[regex]::Match($_.ScaleUnitNode,"scaleUnitNodes/(.*)").Groups[1].value}}
