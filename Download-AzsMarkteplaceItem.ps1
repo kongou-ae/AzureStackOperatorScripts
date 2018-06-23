348e46721648ff12d3e9dacfa292324b0694bd7b
kongou-ae <y.matsumoto.ae@gmail.com>
2018-06-23 15:50:34 +0900

    Update Download-AzsMarkteplaceItem.ps1

--- Download-AzsMarkteplaceItem.ps1
+++ Download-AzsMarkteplaceItem.ps1
@@ -16,21 +16,25 @@ Try {
 
 $resourceGroupName = 'azurestack-activation'
 $activation = Get-AzsAzureBridgeActivation -ResourceGroupName $resourceGroupName 
-
-Write-Output "Downloading the list about marketplace items."
+Write-Output "Downloading list of marketplace items."
 $list = Get-AzsAzureBridgeProduct -ActivationName $activation.Name -ResourceGroupName $resourceGroupName
 $item = $list | Select-Object DisplayName,PublisherDisplayName,GalleryItemIdentity,PayloadLength,Id | Out-GridView -PassThru
 
-# check
-$downloadedItem = Get-AzsAzureBridgeDownloadedProduct -ActivationName $activation.Name -ResourceGroupName $resourceGroupName
-$downloadedItem | foreach {
-    if ( $_.GalleryItemIdentity -eq $item.GalleryItemIdentity ){
-        Write-Error $targetItem + " already downloaded"
-        exit
+if($item){
+    $downloadedItem = Get-AzsAzureBridgeDownloadedProduct -ActivationName $activation.Name -ResourceGroupName $resourceGroupName
+
+    # check
+    $downloadedItem | foreach {
+        if ( $_.GalleryItemIdentity -eq $item.GalleryItemIdentity ){
+            Write-Error "$($item.DisplayName) already downloaded"
+            exit
+        }
     }
+
+    Write-Output "The following item will be downloaded. Please check marketplace after several hours."
+    $item
+
+    Invoke-AzsAzureBridgeProductDownload -ResourceId $item.Id -AsJob -Force | Out-Null
+} else {
+    Write-Output "Please select item."
 }
-
-Write-Output "The following item will be downloaded. Please check marketplace after several hours."
-$item
-
-Invoke-AzsAzureBridgeProductDownload -ResourceId $item.Id -AsJob -Force | Out-Null
