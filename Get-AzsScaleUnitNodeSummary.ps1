c4b1aaec70652d061fbc2c6ad4d0340942113808
kongou-ae <y.matsumoto.ae@gmail.com>
2018-06-24 21:49:27 +0900

    Update Get-AzsScaleUnitNodeSummary.ps1

--- Get-AzsScaleUnitNodeSummary.ps1
+++ Get-AzsScaleUnitNodeSummary.ps1
@@ -1,4 +1,12 @@
 $ErrorActionPreference = "stop"
+
+Try {
+    Get-AzsLocation | Out-Null
+} Catch {
+    Write-Error "Please login Azure Stack before you execute this script" 
+    Break 
+}
+
 $nodes = Get-AzsScaleUnitNode
 $nodes |
     Select-Object Name, ScaleUnitName, ScaleUnitNodeStatus, PowerState, `
