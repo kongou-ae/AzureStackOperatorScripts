9b7c28424d9c7c250ba4dd8746c7fccb9081ff17
kongou-ae <y.matsumoto.ae@gmail.com>
2018-06-24 20:56:56 +0900

    Update Login-AzsAdminRmAccount.ps1

--- Login-AzsAdminRmAccount.ps1
+++ Login-AzsAdminRmAccount.ps1
@@ -8,13 +8,7 @@ Param(
   [parameter(mandatory = $true)][String]$fqdn
 )
 
-# initialize context
-Try {
-    Get-AzureRmContext -ListAvailable | Logout-AzureRmAccount | Out-Null
-}
-catch {
-        
-}
+$ErrorActionPreference = "stop"
 
 Write-Output "Installing modules"
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted 
@@ -22,8 +16,6 @@ Install-Module -Name AzureRm.BootStrapper -Scope CurrentUser -AllowClobber
 Use-AzureRmProfile -Profile 2017-03-09-profile -Force -Scope CurrentUser 
 Install-Module -Name AzureStack -RequiredVersion 1.3.0 -Scope CurrentUser -AllowClobber
 
-Write-Output "Please input your account to login Azure Stack Admin Portal"
-$cred = Get-Credential
 $ArmEndpoint = "https://adminmanagement." + $fqdn
 
 # Register an AzureRM environment that targets your Azure Stack instance
@@ -31,7 +23,7 @@ Add-AzureRMEnvironment `
   -Name "AzureStackAdmin" `
   -ArmEndpoint $ArmEndpoint
 
+Write-Output "Try login to $ArmEndpoint. Please input your account to login Azure Stack Admin Portal"
 # Sign in to your environment
-Login-AzureRmAccount `
-  -EnvironmentName "AzureStackAdmin" `
-  -Credential $cred
+login-AzureRmAccount `
+  -EnvironmentName "AzureStackAdmin"
