c0ff7bcbffc2109396b869261932c9da1f318455
kongou-ae <y.matsumoto.ae@gmail.com>
2019-08-21 23:17:39 +0900

    Update Get-AzsAppServiceRoleLogs.ps1

--- Get-AzsAppServiceRoleLogs.ps1
+++ Get-AzsAppServiceRoleLogs.ps1
@@ -12,47 +12,43 @@ function detectRole($roleNumber){
     return $roleName
 }
 
-$contexts = Get-AzureRmContext
-$context = $contexts.TokenCache.ReadItems() | Where-Object {$_.Resource -like "*adminmanagement*"} | Select-Object -First 1
+$azContext = Get-AzureRMContext
+$azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)
+$token = $profileClient.AcquireAccessToken($azContext.Tenant.TenantId)
 
-$azsEnv = Get-AzureRmEnvironment | Where-Object {$_.ResourceManagerUrl -like "*adminmanagement*"} 
-$fqdn = $azsEnv.ResourceManagerUrl
-
-$azsSub = Get-AzureRmSubscription -SubscriptionName "Default Provider Subscription"
-$subId = $azsSub.Id
-
-$authHeader = "Bearer " + $context.AccessToken
+$authHeader = "Bearer " + $token.AccessToken
 $requestHeader = @{
     "Authorization" = $authHeader
     "Accept" = "application/json"
 }
-$contentType = "application/json;charset=utf-8"
 
+$subId = (Get-AzureRmSubscription).Id
+$region = (Get-AzsRegionHealth).Name
+$adminManagementUrl = (Get-AzureRmEnvironment -Name AzureStackAdmin).ResourceManagerUrl
 
 $saveDir = $env:SystemDrive + "\Get-AzsAppServiceRoleLogs-"  + (Get-Date -Format yyyyMMdd-hhmmss)
 mkdir $saveDir | Out-Null
 
 $logFilename = $saveDir + "\" + (detectRole($_.properties.role)) + "servers.json"   
-$Url = $fqdn + "/subscriptions/" + $subId + "/providers/Microsoft.Web.Admin/locations/local/servers?api-version=2018-02-01"
+$Url = $adminManagementUrl + "/subscriptions/" + $subId + "/providers/Microsoft.Web.Admin/locations/$region/servers?api-version=2018-02-01"
 $res = Invoke-RestMethod -Uri $Url -Method GET -Headers $requestHeader -ContentType $contentType
 $res | ConvertTo-Json -Depth 100 | Out-File $logFilename
 Write-Output "Created $logFilename"
 
 $logFilename = $saveDir + "\" + (detectRole($_.properties.role)) + "workerTiers.json"   
-$Url = $fqdn + "/subscriptions/" + $subId + "/providers/Microsoft.Web.Admin/locations/local/workerTiers?api-version=2018-02-01"
+$Url = $adminManagementUrl + "/subscriptions/" + $subId + "/providers/Microsoft.Web.Admin/locations/$region/workerTiers?api-version=2018-02-01"
 $res = Invoke-RestMethod -Uri $Url -Method GET -Headers $requestHeader -ContentType $contentType
 $res | ConvertTo-Json -Depth 100 | Out-File $logFilename
 Write-Output "Created $logFilename"
 
-$serverUrl = $fqdn + "/subscriptions/" + $subId + "/providers/Microsoft.Web.Admin/locations/local/servers?api-version=2018-02-01"
+$serverUrl = $adminManagementUrl + "/subscriptions/" + $subId + "/providers/Microsoft.Web.Admin/locations/$region/servers?api-version=2018-02-01"
 $serverRes = Invoke-RestMethod -Uri $serverUrl -Method GET -Headers $requestHeader -ContentType $contentType
 $serverRes | foreach {
-    $logUrl = $fqdn + "/subscriptions/" + $subId + "/providers/Microsoft.Web.Admin/locations/local/servers/" + $_.name  + "/log?api-version=2018-02-01"
+    $logUrl = $adminManagementUrl + "/subscriptions/" + $subId + "/providers/Microsoft.Web.Admin/locations/$region/servers/" + $_.name  + "/log?api-version=2018-02-01"
     $logRes = Invoke-RestMethod -Uri $logUrl -Method GET -Headers $requestHeader -ContentType $contentType
  
     $logFilename = $saveDir + "\" + (detectRole($_.properties.role)) + "-" + $_.name + ".json"   
     $logRes.properties | ConvertTo-Json -Depth 100 | Out-File $logFilename
     Write-Output "Created $logFilename"
 }
-
-
